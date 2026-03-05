acf72846e3d0e2330179147654212728cba9231e
Pedro Porto <pedrotovarporto@gmail.com>
2026-03-05 16:32:26 +0000

    fix: use @prisma/client/wasm for better worker compatibility

--- src/lib/shopify.ts
+++ src/lib/shopify.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/wasm";
 
 export async function syncShopifyOrders(
     prisma: InstanceType<typeof PrismaClient>,
