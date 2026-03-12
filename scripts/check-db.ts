a4a45afbaab57a0bab1ace0ca70d8e577b953a00
Pedro Porto <pedrotovarporto@gmail.com>
2026-03-12 19:38:14 +0000

    security: remove sensitive scripts from repository

--- scripts/check-db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-    const count = await prisma.gcalStaff.count();
-    console.log("GcalStaff count:", count);
-}
-main();
